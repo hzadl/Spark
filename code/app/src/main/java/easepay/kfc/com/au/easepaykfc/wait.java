062aa98fc3621d373ccca2d97e79c2c5b5e4372b
superpower1 <superpiglet1@gmail.com>
2015-07-22 17:07:19 +0930

    add wait act
    add

--- code/app/src/main/java/easepay/kfc/com/au/easepaykfc/wait.java
+++ code/app/src/main/java/easepay/kfc/com/au/easepaykfc/wait.java
@@ -1,25 +1,23 @@
 package easepay.kfc.com.au.easepaykfc;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class wait extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_wait);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_wait, menu);
         return true;
     }
 
@@ -37,17 +35,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    public void collectBtnClicked(View view){
-        System.out.println("collect clicked");
-        Intent i = new Intent(this, inputOrderNumberActivity.class);
-        startActivity(i);
-    }
-
-    public void newBtnClicked(View view){
-        System.out.println("new clicked");
-//        Intent i = new Intent(getApplicationContext(), OrderInfoActivity.class);
-//        startActivity(i);
-    }
-
 }
