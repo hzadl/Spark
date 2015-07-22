774d06105f7a263d9fea998f745a8f21d8afc4fc
Hao Zhang <hyjzhang@gmail.com>
2015-07-23 08:17:46 +0930

    Hao: add Order model

---
+++ code/app/src/main/java/easepay/kfc/com/au/easepaykfc/easepay/kfc/com/au/easepaykfc/model/Order.java
@@ -0,0 +1,33 @@
+package easepay.kfc.com.au.easepaykfc.easepay.kfc.com.au.easepaykfc.model;
+
+import java.util.List;
+
+/**
+ * Created by Kodo on 23/07/15.
+ */
+public class Order {
+    List<Product> products;
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setIsPaid(boolean isPaid) {
+        this.isPaid = isPaid;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    boolean isPaid;
+    public Order(List<Product> products,boolean isPaid){
+        this.products = products;
+        this.isPaid = isPaid;
+    }
+
+}
