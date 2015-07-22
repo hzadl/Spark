1c6729f9bce26d8c5dd444f1747e61a9c399f831
superpower1 <superpiglet1@gmail.com>
2015-07-22 17:38:35 +0930

    modify

--- code/app/src/main/java/easepay/kfc/com/au/easepaykfc/wait.java
+++ code/app/src/main/java/easepay/kfc/com/au/easepaykfc/wait.java
@@ -1,5 +1,6 @@
 package easepay.kfc.com.au.easepaykfc;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -38,6 +39,8 @@ public class wait extends ActionBarActivity {
     }
 
     public void okBtnClicked(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
         finish();
     }
 }
